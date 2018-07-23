aad21f76024089014267042678f681834215e81e
wl <360601917@qq.com>
2018-07-23 21:13:29 +0800

    a

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,25 +4,25 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
-class AppServiceProvider extends ServiceProvider
-{
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        //
-    }
+class AppServiceProvider extends ServiceProvider {
 
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        //
-    }
+
+  /**
+   * Bootstrap any application services.
+   *
+   * @return void
+   */
+  public function boot() {
+    \Carbon\Carbon::setLocale('zh');
+  }
+
+  /**
+   * Register any application services.
+   *
+   * @return void
+   */
+  public function register() {
+    //
+  }
+
 }
