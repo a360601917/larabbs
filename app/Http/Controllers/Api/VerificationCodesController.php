c93f76b7a010b0016ef5c0d93b6312aa9f34542f
wl1 <360601917@qq.com>
2018-08-10 11:11:52 +0800

    a

--- app/Http/Controllers/Api/VerificationCodesController.php
+++ app/Http/Controllers/Api/VerificationCodesController.php
@@ -11,13 +11,23 @@ use App\Http\Requests\Api\VerificationCodeRequest;
 class VerificationCodesController extends Controller {
 
   public function store(VerificationCodeRequest $request, EasySms $easySms) {
-    $phone = $request->phone;
-    
+    $captchaData = \Cache::get($request->captcha_key);
+
+    if (!$captchaData) {
+      return $this->response->error('图片验证码已失效', 422);
+    }
+
+    if (!hash_equals($captchaData['code'], $request->captcha_code)) {
+      // 验证错误就清除缓存
+      \Cache::forget($request->captcha_key);
+      return $this->response->errorUnauthorized('验证码错误');
+    }
+
+    $phone = $captchaData['phone'];
+
     if (!app()->environment('production')) {
-      //不用真实发送
       $code = '1234';
     } else {
-
       // 生成4位随机数，左侧补0
       $code = str_pad(random_int(1, 9999), 4, 0, STR_PAD_LEFT);
 
@@ -28,9 +38,10 @@ class VerificationCodesController extends Controller {
                 'code' => $code
             ],
         ]);
-      } catch (\Overtrue\EasySms\Exceptions\NoGatewayAvailableException $exception) {
-        $message = $exception->getException('aliyun')->getMessage();
-        return $this->response->errorInternal($message ?? '短信发送异常');
+      } catch (\GuzzleHttp\Exception\ClientException $exception) {
+        $response = $exception->getResponse();
+        $result = json_decode($response->getBody()->getContents(), true);
+        return $this->response->errorInternal($result['msg'] ?? '短信发送异常');
       }
     }
 
@@ -38,6 +49,8 @@ class VerificationCodesController extends Controller {
     $expiredAt = now()->addMinutes(10);
     // 缓存验证码 10分钟过期。
     \Cache::put($key, ['phone' => $phone, 'code' => $code], $expiredAt);
+    // 清除图片验证码缓存
+    \Cache::forget($request->captcha_key);
 
     return $this->response->array([
                 'key' => $key,
