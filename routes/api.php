de31aac710efe0d7b3fc86b6b30ec936e353ee22
wl1 <360601917@qq.com>
2018-08-10 10:13:52 +0800

    a

--- routes/api.php
+++ routes/api.php
@@ -3,15 +3,15 @@
 use Illuminate\Http\Request;
 
 /*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+  |--------------------------------------------------------------------------
+  | API Routes
+  |--------------------------------------------------------------------------
+  |
+  | Here is where you can register API routes for your application. These
+  | routes are loaded by the RouteServiceProvider within a group which
+  | is assigned the "api" middleware group. Enjoy building your API!
+  |
+ */
 
 //Route::middleware('auth:api')->get('/user', function (Request $request) {
 //    return $request->user();
@@ -20,8 +20,11 @@ $api = app('Dingo\Api\Routing\Router');
 
 $api->version('v1', [
     'namespace' => 'App\Http\Controllers\Api'
-], function($api) {
-    // 短信验证码
-    $api->post('verificationCodes', 'VerificationCodesController@store')
-        ->name('api.verificationCodes.store');
+        ], function($api) {
+  // 短信验证码
+  $api->post('verificationCodes', 'VerificationCodesController@store')
+          ->name('api.verificationCodes.store');
+  // 用户注册
+  $api->post('users', 'UsersController@store')
+          ->name('api.users.store');
 });
