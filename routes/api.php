e5a5d51c6419e64c7ec373a850835a26512a424f
wl1 <360601917@qq.com>
2018-08-14 11:04:54 +0800

    a

--- routes/api.php
+++ routes/api.php
@@ -50,7 +50,7 @@ $api->version('v1', [
     $api->delete('authorizations/current', 'AuthorizationsController@destroy')
             ->name('api.authorizations.destroy');
   });
-  
+
   $api->group([
       'middleware' => 'api.throttle',
       'limit' => config('api.rate_limits.access.limit'),
@@ -62,8 +62,12 @@ $api->version('v1', [
       // 当前登录用户信息
       $api->get('user', 'UsersController@me')
               ->name('api.user.show');
+      // 编辑登录用户信息
+      $api->patch('user', 'UsersController@update')
+              ->name('api.user.update');
+      // 图片资源
+      $api->post('images', 'ImagesController@store')
+              ->name('api.images.store');
     });
   });
-  
-  
 });
