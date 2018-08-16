54165bcfcec63653f8bf8aa7f6259ac0b680e50c
wl1 <360601917@qq.com>
2018-08-16 10:20:19 +0800

    a

--- routes/api.php
+++ routes/api.php
@@ -62,8 +62,16 @@ $api->version('v1', [
             ->name('api.categories.index');
     $api->get('topics', 'TopicsController@index')
             ->name('api.topics.index');
+    // 某个用户发布的话题
     $api->get('users/{user}/topics', 'TopicsController@userIndex')
             ->name('api.users.topics.index');
+    // 话题回复列表
+    $api->get('topics/{topic}/replies', 'RepliesController@index')
+            ->name('api.topics.replies.index');
+    // 某个用户的回复列表
+    $api->get('users/{user}/replies', 'RepliesController@userIndex')
+            ->name('api.users.replies.index');
+
     $api->get('topics/{topic}', 'TopicsController@show')
             ->name('api.topics.show');
 
@@ -92,6 +100,15 @@ $api->version('v1', [
       // 删除回复
       $api->delete('topics/{topic}/replies/{reply}', 'RepliesController@destroy')
               ->name('api.topics.replies.destroy');
+      // 通知列表
+      $api->get('user/notifications', 'NotificationsController@index')
+              ->name('api.user.notifications.index');
+      // 通知统计
+      $api->get('user/notifications/stats', 'NotificationsController@stats')
+              ->name('api.user.notifications.stats');
+      // 标记消息通知为已读
+      $api->patch('user/read/notifications', 'NotificationsController@read')
+              ->name('api.user.notifications.read');
     });
   });
 });
