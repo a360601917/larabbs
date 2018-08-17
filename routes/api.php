a2b2be39bbca152cf487e1b83756cb138e222f34
wl1 <360601917@qq.com>
2018-08-17 10:46:08 +0800

    a

--- routes/api.php
+++ routes/api.php
@@ -20,7 +20,7 @@ $api = app('Dingo\Api\Routing\Router');
 
 $api->version('v1', [
     'namespace' => 'App\Http\Controllers\Api',
-    'middleware' => ['serializer:array', 'bindings']
+    'middleware' => ['serializer:array', 'bindings','change-locale']
         ], function($api) {
 
   $api->group([
@@ -74,6 +74,12 @@ $api->version('v1', [
 
     $api->get('topics/{topic}', 'TopicsController@show')
             ->name('api.topics.show');
+    // 资源推荐
+    $api->get('links', 'LinksController@index')
+            ->name('api.links.index');
+    // 活跃用户
+    $api->get('actived/users', 'UsersController@activedIndex')
+            ->name('api.actived.users.index');
 
 
     // 需要 token 验证的接口
